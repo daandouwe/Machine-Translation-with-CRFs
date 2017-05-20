77b24f4589d84f892d0f962a009c919868a2d070
Daan van Stigt <daan@Daans-MacBook-Pro.local>
2017-05-20 14:09:35 +0200

    SGD can now also predict every iteration.
    Useful for hyperparameter inference.

--- train.py
+++ train.py
@@ -4,12 +4,15 @@ from collections import defaultdict
 from processing import *
 from features import featurize_edges, get_full_fset
 from util import save_weights, load_weights, partition
+from predict import predict
+
 
 # savepath = '../parses/29-sents-2-translations-sparse/'
 # savepath = '../parses/eps-100/'
 savepath = '../parses/eps-2k/'
+predictpath =  'prediction/2k/'
 
-parses = [load_parses_separate(savepath, k) for k in range(200)]
+parses = [load_parses_separate(savepath, k) for k in range(60)]
 
 lexicon = load_lexicon(savepath)
 
@@ -29,18 +32,15 @@ w_init = defaultdict(float)
 for feature in fset:
     w_init[feature] = 1e-2
 
-# partition the parses in minibatches each of size 15 (e.g.)
-minibatches = partition(parses, 30)
-w_trained, delta_ws = sgd_minibatches(4, 20, w_init, minibatches=minibatches, 
-                                      sparse=True, bar=True, log=False, log_last=True,
-                                      check_convergence=True, scale_weight=3, regularizer=False,
-                                      lmbda=0.2, savepath='../parses/eps-2k/weights/lr-20/')
 
-save_weights(w_trained, '../parses/eps-2k/weights/lr-20/trained-')
+# partition the parses in minibatches each of size 15 (e.g.)
+minibatches = partition(parses, 20)
+w_trained, delta_ws = sgd_minibatches(30, 5, w_init, minibatches=minibatches, 
+                                      sparse=True, bar=True, log=False, log_last=True,
+                                      check_convergence=True, scale_weight=1, regularizer=False,
+                                      lmbda=0.2, savepath=savepath, prediction=predictpath)
 
 # printing for verification
 w = w_trained[-1]
-for k in sorted(w.keys()):
-	print('{}'.format(k).ljust(25) + '{}'.format(w[k]))
-
-
+for k, v in sorted(w.items(), key=lambda x: x[1], reverse=True):
+	print('{}'.format(k).ljust(25) + '{}'.format(v))
